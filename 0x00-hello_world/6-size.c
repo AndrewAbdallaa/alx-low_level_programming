9cfacd9600069242f7f96a7e9c24300531c8a69d
andrew <andrew.abdallaa@gmail.com>
2023-02-16 16:10:15 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - Program that prints size
+ * Description: A progam that print the size
+ * Retrun: 0 (Success)
+ */
+int main(void)
+{
+printf("char size: %lu bytes\n", sizeof(char));
+printf("int size: %lu bytes\n", sizeof(int));
+printf("long int size: %lu bytes\n", sizeof(long int));
+printf("long long int size: %lu bytes\n", sizeof(long long int));
+printf("float size: %lu bytes\n", sizeof(float));
+return (0);
+}
